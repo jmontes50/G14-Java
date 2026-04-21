12abff96a0953e9b37468959d0403ee08f63fcb7
Jorge Montesinos <osmarmontes50@gmail.com>
2026-04-20 19:33:30 -0500

    avances

---
+++ 04-javascript/01-introduccion.js
@@ -0,0 +1,23 @@
+/* Comentarios
+de
+bloque */
+//Comentarios de linea
+
+console.log("Hola mundo desde JS!");
+
+//en JS no es necesario declarar el tipo de variable, se infiere automaticamente
+let edad = 30;
+
+console.log(typeof edad);
+
+edad = "treinta"; //esto es posible pero no recomendable, estamos cambiando el tipo de dato de la variable
+
+console.log(typeof edad);
+
+let hacefrio = true;
+
+console.log(typeof hacefrio);
+
+let noDefinido;
+
+console.log(typeof noDefinido);
