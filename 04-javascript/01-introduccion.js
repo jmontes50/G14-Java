60991b57bb37650342d3feb5c0f117544211e1b1
Jorge Montesinos <osmarmontes50@gmail.com>
2026-04-20 19:53:45 -0500

    avances

--- 04-javascript/01-introduccion.js
+++ 04-javascript/01-introduccion.js
@@ -29,7 +29,7 @@ console.log(esNulo);
 console.log(typeof esNulo); //esto es un error en JS, el tipo de dato de null es object, pero en realidad no es un objeto, es un valor primitivo que representa la ausencia de valor
 
 //es el problema de concatenar
-let nota = 20;
+let nota = 20; //para crear una variable se usa la palabra reservada let, var o const, se recomienda usar let o const, var es una forma antigua de declarar variables y tiene un comportamiento diferente al de let y const, se recomienda no usar var
 let nota2 = "10";
 
 console.log(nota + nota2); //esto no es una suma, es una concatenacion, el resultado es "2010", se usa el símbolo + para concatenar
@@ -43,3 +43,12 @@ console.log(parseFloat(nota2)); //esto también es una forma de parsear, el resu
 
 let texto = "Hola!";
 console.log("isNotANumber resultado:",isNaN(texto)); //isNaN() es una función que devuelve true si el valor no es un número, en este caso devuelve true porque "Hola!" no es un número
+
+//Expresiones
+texto
+10 + 20
+isNaN("25")
+
+//Sentencia
+texto = "Moin";
+console.log("Hola mundo!"); //esto es una sentencia, es una instrucción que el programa ejecuta, una sentencia puede contener una o varias expresiones, en este caso la expresión es "Hola mundo!" y la sentencia es console.log("Hola mundo!");
