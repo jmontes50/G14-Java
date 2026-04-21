ec9f33a5cabda909189215be9a5f2619075e67ad
Jorge Montesinos <osmarmontes50@gmail.com>
2026-04-20 20:27:35 -0500

    avances

---
+++ 04-javascript/02-arraysyobjetos.js
@@ -0,0 +1,14 @@
+let numeros = [1, 2, 3, 4, 5]; //esto es un array, un array es una estructura de datos que puede contener varios valores, en este caso el array numeros contiene los valores 1, 2, 3, 4 y 5
+
+console.log(numeros);
+
+let frutas = ["manzana", "banana", "naranja"];
+
+console.log(frutas);
+
+console.log(frutas[2]); //para acceder a un elemento del array se usa el índice, el índice empieza en 0, en este caso el resultado es "naranja"
+
+console.table(frutas); //console.table() es una función que muestra los datos en forma de tabla, en este caso muestra el array frutas en forma de tabla
+
+let arrMultiple = [1, "hola", true, null, undefined, [1, 2, 3]]; //un array puede contener cualquier tipo de dato, incluso otros arrays o objetos
+console.table(arrMultiple);
