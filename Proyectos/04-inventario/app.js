2ca6fb3c9e20434dea266f4dbb63eccb8a966924
Jorge Montesinos <osmarmontes50@gmail.com>
2026-04-22 22:49:51 -0500

    avances

--- Proyectos/04-inventario/app.js
+++ Proyectos/04-inventario/app.js
@@ -1,7 +1,7 @@
 let productos = [
-    { id: 1, nombre: "Laptop", precio: 1200, cantidad: 10 },
-    { id: 2, nombre: "Smartphone", precio: 800, cantidad: 20 },
-    { id: 3, nombre: "Tablet", precio: 500, cantidad: 15 }
+  { id: 1, nombre: "Laptop", precio: 1200, cantidad: 10 },
+  { id: 2, nombre: "Smartphone", precio: 800, cantidad: 20 },
+  { id: 3, nombre: "Tablet", precio: 500, cantidad: 15 }
 ];
 
 function mostrarInventario() {
@@ -12,24 +12,38 @@ function mostrarInventario() {
 }
 
 const verTotalInventario = () => {
-    const total = productos.reduce((acumulador, producto) => {
-        return acumulador + (producto.precio * producto.cantidad);
-    }, 0);
-    return `El valor total del inventario es: $${total}`;
+  const total = productos.reduce((acumulador, producto) => {
+    return acumulador + (producto.precio * producto.cantidad);
+  }, 0);
+  return `El valor total del inventario es: $${total}`;
 };
 
 const opciones = {
-    1: mostrarInventario,
-    2: verTotalInventario
+  1: mostrarInventario,
+  2: verTotalInventario
 }
 
 const menu = () => {
+  let continuar = true;
+  do {
+    //do ejecuta el bloque al menos una vez
     const opcion = prompt(`
       Seleccione una opción:
         1. Mostrar Inventario
-        2. Ver Total del Inventario`);
-        console.log(opcion)
-        console.log(typeof opcion);
+        2. Ver Total del Inventario
+        0: Salir
+    `);
+    console.log(opcion)
+    console.log(typeof opcion);
+    if (opcion === "0") { //cerramos el programa
+      continuar = false;
+    } else if (opciones[opcion]) { //truthy si la opción existe en el objeto opciones
+      alert(opciones[opcion]());
+    } else {
+      alert("Opción no válida, por favor intente de nuevo.");
+    }
+    //mientras la condición sea verdadera
+  } while (continuar)
 }
 
 menu();
