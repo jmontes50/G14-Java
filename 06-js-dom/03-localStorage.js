57d64aaec2cb87e0f78e9440f25a1e77fd0bfdca
Jorge Montesinos <osmarmontes50@gmail.com>
2026-04-27 22:02:54 -0500

    avances

--- 06-js-dom/03-localStorage.js
+++ 06-js-dom/03-localStorage.js
@@ -1,9 +1,27 @@
 const form = document.getElementById("form-suscripcion");
 const inputEmail = document.getElementById("input-email");
 
+const arrCorreos = [];
+
 form.addEventListener("submit", (evento) => {
   //previene el evento por defecto de un elemento ej submit recargandose, hipervnculo abriendo una dirección
   evento.preventDefault();
-  console.log("Submit!!!!")
-  console.log("holaaaaa")
+  // console.log("Submit!!!!");
+  // console.log("holaaaaa");
+
+  const nuevoCorreo = inputEmail.value;
+  console.log({ nuevoCorreo });
+
+  arrCorreos.push(nuevoCorreo);
+
+  console.log({ arrCorreos });
+
+  const jsonCorreos = JSON.stringify(arrCorreos);
+
+  console.log({ jsonCorreos });
+
+  //setItem clave , valor . solamente se puede guardar strings.
+  localStorage.setItem("correos", jsonCorreos);
+
+
 })
