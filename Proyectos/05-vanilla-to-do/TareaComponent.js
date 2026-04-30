6d11d63bca50d83f131c66a89b282bfe965c7188
Jorge Montesinos <osmarmontes50@gmail.com>
2026-04-29 20:13:41 -0500

    avance

---
+++ Proyectos/05-vanilla-to-do/TareaComponent.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param {object} objTarea tiene descripcion y estado
+ * @returns {object} de tipo Element que representa un li
+ */
+const TareaComponent = (objTarea) => {
+  const liTarea = document.createElement("li");
+  liTarea.innerHTML = `
+  <li class="list-row flex gap-2">
+    <span>${objTarea.descripcion}</span>
+    <button class="btn btn-sm">Finalizar</button>
+  </li>
+  `;
+
+
+  return liTarea;
+}
+
+export default TareaComponent;
