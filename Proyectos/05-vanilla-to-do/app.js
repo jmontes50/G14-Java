58db64ae5df9efc0ce6c9b04f5aa846de1296601
Jorge Montesinos <osmarmontes50@gmail.com>
2026-04-29 20:06:11 -0500

    avance

--- Proyectos/05-vanilla-to-do/app.js
+++ Proyectos/05-vanilla-to-do/app.js
@@ -5,10 +5,22 @@ const ulTareas = document.querySelector("#tareas");
 // console.log({ inputTarea, btnTarea, ulTareas })
 
 const App = () => {
+  const listaTareas = [];
 
   btnTarea.addEventListener("click", () => {
     const textoTarea = inputTarea.value;
     console.log(textoTarea);
-  })
+
+    const nuevaTarea = {
+      descripcion: textoTarea,
+      estado: false
+    }
+
+    listaTareas.push(nuevaTarea);
+    inputTarea.value = "";
+
+    console.table(listaTareas);
+  });
+
 }
 App();
