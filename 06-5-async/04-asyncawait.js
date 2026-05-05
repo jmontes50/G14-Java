ce2f6affa3767e40bc5519bf4f34d290e9611e13
Jorge Montesinos <osmarmontes50@gmail.com>
2026-05-04 20:25:42 -0500

    avance

--- 06-5-async/04-asyncawait.js
+++ 06-5-async/04-asyncawait.js
@@ -1,9 +1,9 @@
-// const tramite = () => {
-//   return new Promise((resolve, reject) => {
-//     resolve("Trámite exitoso");
-//     // reject("Trámite fallido")
-//   })
-// }
+const tramite = () => {
+  return new Promise((resolve, reject) => {
+    resolve("Trámite exitoso");
+    // reject("Trámite fallido")
+  })
+}
 
 // const tramite = async () => {
 //   // return "Trámite exitoso";
@@ -13,20 +13,33 @@
 //   console.log("holaaaa")
 // }
 
-async function tramite () {
-  const resultado = Math.random();
-  console.log({ resultado })
-  if(resultado >= 0.5) {
-    return "Trámite exitoso"; //resolve
-  }else {
-    throw "trámite fallido"; //reject
+// async function tramite () {
+//   const resultado = Math.random();
+//   console.log({ resultado })
+//   if(resultado >= 0.5) {
+//     return "Trámite exitoso"; //resolve
+//   }else {
+//     throw "trámite fallido"; //reject
+//   }
+// }
+
+// capturando el resultado de la promesa con then y catch
+// tramite()
+// .then((resultado) => {
+//   console.log(resultado);
+// })
+// .catch((error) => {
+//   console.log(error)
+// })
+
+
+const obtenerResultado = async () => {
+  //trycatch capturar el resultado de promesas y capturar excepciones/errores
+  try { //intenta esto
+    const resultadoTramite = await tramite(); //then
+    console.log({ resultadoTramite });
+  } catch (error) { //en caso que falle, catch
+    console.log({ error })
   }
 }
-
-tramite()
-.then((resultado) => {
-  console.log(resultado);
-})
-.catch((error) => {
-  console.log(error)
-})
+obtenerResultado();
