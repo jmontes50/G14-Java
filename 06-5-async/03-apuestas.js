a9b43ea4f8d9d7e4f8d9650cc6a24a805e96d44b
Jorge Montesinos <osmarmontes50@gmail.com>
2026-05-04 19:49:26 -0500

    avance

--- 06-5-async/03-apuestas.js
+++ 06-5-async/03-apuestas.js
@@ -8,18 +8,36 @@ const UTCvsUNIVERSITARIO = (equipo) => {
       console.log({ resultado })
       const ganador = resultado < 0.5 ? "UTC" : "UNIVERSITARIO";
       if(ganador === equipo) {
-        resolve("Ganaste la apuesta!");
+        resolve("Ganaste la apuesta 1!");
       }else {
-        reject("Perdiste la apuesta");
+        reject("Perdiste la apuesta 1");
       }
     }, 2000);
   })
 }
 
-UTCvsUNIVERSITARIO("UNIVERSITARIO")
-.then((resPositivo) => { //resolve
-  console.log({ resPositivo })
+const SCvsALIANZA = (equipo) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const resultado = Math.random(); //entre 0 y 1, 0.4 0.2 0.78
+      console.log({ resultado })
+      const ganador = resultado < 0.5 ? "SC" : "ALIANZA";
+      if(ganador === equipo) {
+        resolve("Ganaste la apuesta 2!");
+      }else {
+        reject("Perdiste la apuesta 2");
+      }
+  }, 3000)
 })
-.catch((resNegativo) => { //reject
+
+UTCvsUNIVERSITARIO("UNIVERSITARIO")
+.then((resPositivo) => { //resolve UTCvsUNIVERSITARIO
+  console.log({ resPositivo })
+  return SCvsALIANZA("SC");
+})
+.then((resPositivo2) => { //resolve SCvsALIANZA
+  console.log({ resPositivo2 });
+  //return nuevaPromesa
+})
+.catch((resNegativo) => { //reject, de las promesas que tengamos arriba
   console.error({ resNegativo })
 })
