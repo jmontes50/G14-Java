74685c008aac51b48d888bb945886e52b29b07d2
Jorge Montesinos <osmarmontes50@gmail.com>
2026-05-06 20:37:36 -0500

    avance

---
+++ 08-react/01-introreact/src/App.jsx
@@ -0,0 +1,17 @@
+/**
+ * 1. la función que representa un componente de react debe iniciar con mayuscula
+ * 2. debe retornar un elemento jsx/html
+ * 3. debe ser exportado para ser utilizado en otros archivos
+ * 4. debe ser importado en el archivo donde se va a utilizar
+ */
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+    </div>
+  )
+}
+
+export default App;
