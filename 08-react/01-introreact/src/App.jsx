4a64beea349314ee0d0af86b928055d47046c37d
Jorge Montesinos <osmarmontes50@gmail.com>
2026-05-06 20:51:46 -0500

    avance

--- 08-react/01-introreact/src/App.jsx
+++ 08-react/01-introreact/src/App.jsx
@@ -6,11 +6,29 @@
  */
 
 const App = () => {
+  const titulo = "Hola Mundo pero soy una variable";
 
+  // jsx es javascript extension, le da la posibilidad de escribir una sintaxis similar a html dentro de javascript, pero no es html
+  /**
+   * 1. no se pueden retornar varios elementos, deben estar envueltos en un contenedor padre
+   * 2. todos los elementos deben cerrarse, incluso los elementos vacíos como <hr /> o <img />
+   * 3. podemos usar llaves {} para insertar expresiones de javascript dentro del jsx, como variables, operaciones matemáticas, funciones, etc.
+   * 4. uso de atributos class o for, en jsx se usan className y htmlFor respectivamente para evitar conflictos con las palabras reservadas de javascript
+   */
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <h1 className="titulo">{titulo}</h1>
+      <p>Bienvenidos a React</p>
+      <p>Multiplicación: {5 * 10 * 27}</p>
+      <hr />
+      <img src="https://picsum.photos/id/237/200/300" alt="animal" />
+      <hr />
+      <div>
+        <label htmlFor="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" />
+      </div>
     </div>
+
   )
 }
 
