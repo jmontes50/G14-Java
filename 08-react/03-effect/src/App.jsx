060f854fb4953919c18cb9dcb89a531e993e9ce1
Jorge Montesinos <osmarmontes50@gmail.com>
2026-05-08 21:02:23 -0500

    avance

--- 08-react/03-effect/src/App.jsx
+++ 08-react/03-effect/src/App.jsx
@@ -4,17 +4,21 @@ const App = () => {
   const [logged, setLogged] = useState(false);
   const [counter, setCounter] = useState(1000);
 
+  console.log("1. después de estados!!!!")
+
   const manejarBoton = () => {
     console.log("!!!")
     setLogged(!logged);
   }
 
   useEffect(() => {
+    console.log("2. useEffect!!!!")
     console.log("LOGGED:", logged);
-  }, [])
+  }, [counter])
 
   return (
     <div>
+      {console.log("3. estoy dentro del return")}
       <button onClick={manejarBoton}>Cambiar Status</button>
       Estado: {JSON.stringify(logged)}
       <hr/>
