683d7090097b188d1a942205948185469d1b38e0
Jorge Montesinos <osmarmontes50@gmail.com>
2026-05-08 20:45:36 -0500

    avance

--- 08-react/03-effect/src/App.jsx
+++ 08-react/03-effect/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 
 const App = () => {
   const [logged, setLogged] = useState(false);
+  const [counter, setCounter] = useState(1000);
 
   const manejarBoton = () => {
     console.log("!!!")
@@ -10,12 +11,17 @@ const App = () => {
 
   useEffect(() => {
     console.log("LOGGED:", logged);
-  })
+  }, [])
 
   return (
     <div>
       <button onClick={manejarBoton}>Cambiar Status</button>
       Estado: {JSON.stringify(logged)}
+      <hr/>
+      <p>Contador: {counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>
+        Incrementar!
+      </button>
     </div>
   )
 }
