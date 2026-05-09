cde9c816a39832e05cac6b01b45b3ec77074c9ce
Jorge Montesinos <osmarmontes50@gmail.com>
2026-05-08 19:45:04 -0500

    avance

--- 08-react/02-state/src/App.jsx
+++ 08-react/02-state/src/App.jsx
@@ -7,17 +7,35 @@ const App = () => {
   //const [getter, setter] = useState(valorInicial);
   //porque cada vez que se actualiza el estado, el componente se vuelve a renderizar, entonces el getter siempre va a tener el valor actualizado
   const [contador, setContador] = useState(0);
+  const [busqueda, setBusqueda] = useState("project hail mary");
+
 
   const manejarClick = () => {
     //3. actualizar el estado
     setContador(contador + 1);
   }
 
+  const manejarInput = (evento) => {
+    console.log(evento.target.value);
+    setBusqueda(evento.target.value);
+  }
+
+  console.log(contador + 10)
+
   return (
     <div>
       <h1>State</h1>
       <p>Contador: {contador}</p>
       <button onClick={manejarClick}>Click!</button>
+      <hr />
+      {/* componentes controlados */}
+      <input
+        type="text"
+        value={busqueda}
+        //onChange es un evento que detecta los cambios en un input
+        onChange={manejarInput}
+      />
+
     </div>
   )
 }
