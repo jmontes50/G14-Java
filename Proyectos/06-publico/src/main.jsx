562005faf31be369ccafb1ee25c408070d4cbb3f
Jorge Montesinos <osmarmontes50@gmail.com>
2026-05-15 19:55:46 -0500

    avance

--- Proyectos/06-publico/src/main.jsx
+++ Proyectos/06-publico/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { FavoritesContextProvider } from './context/favoriteContext.jsx'
+import { SearchContextProvider } from './context/searchContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <FavoritesContextProvider>
-      <App />
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
     </FavoritesContextProvider>
   </StrictMode>,
 )
